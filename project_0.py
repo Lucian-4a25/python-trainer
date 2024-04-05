67f7deb9d433769a1eaa09daac1f2714e1707955
Lucian <lucian.journey@gmail.com>
2024-04-05 22:19:26 +0800

    add project 0 learning codes

--- project_0.py
+++ project_0.py
@@ -4,14 +4,41 @@
 # - 如果输入的 num1 和 num2 的乘积小于 1000，那么返回这两个数的乘积
 def multiplication_or_sum(num1, num2):
   # 你的代码写在这里
-  0
+  None
 
 # - 如果输入是偶数则返回 1，如果输入是奇数则返回 2
 # - 如果输入非数字类型的参数，返回 0
 def even_or_odd(n):
   # 你的代码写在这里
-  n
+  None
 
+# 输入 arr 是一个数组，包含的元素为数字；现在要求计算 arr 中所有元素之和
+def add_and_iterate(arr):
+  None
+
+# 输入 arr 是一个包含数字的数组，n 是一个输入为正整数的值；
+# - 要求 输出一个数组，数组的值为 arr 中所有刚好可以整除 n 的元素，如 n 为 5 的时候，
+# arr 为 [10, 12, 20]，那么要求输出为 [10, 20]
+def save_divisible(arr, n):
+  None
+
+# 输入 arr1 和 arr2 都是两个包含数字的数组，n 是一个正整数；
+# - 检查是否 arr1 的前 n 个元素是否等于 arr2 的后 n 个元素；如输入 n 的值为 2 的时候，
+# [1, 2, 3, 4] 的前两个元素等于 [4, 1, 2] 的后两个元素，返回 True, 
+# 但是不等于 [4, 3, 1] 的后两位元素，返回 False
+def check_n_same(arr1, arr2, n):
+  None
+
+# 输入 s 为一个字符串，要求反转这个字符串并返回
+def reverse_str(s):
+  None
+
+# 输入 s 为一个字符串，n 为一个正整数；
+# - 要求输出一个字符串，这个字符串的值为将 s 的前 n 字符移除后的结果；
+# - 如果 n 大于字符串 s 的长度，返回一个空字符串
+# - 如果 n 不是一个数字或是一个负数，抛出异常 TypeError
+def remove_first_n_chars(s, n):
+  None
 
 if __name__ == '__main__':
   print("Hello Python")
--- tests/test_project_0.py
+++ tests/test_project_0.py
@@ -4,17 +4,19 @@ sys.path.append("..")
 import unittest
 
 # print(sys.path)
-from project_0 import even_or_odd, multiplication_or_sum
+from project_0 import even_or_odd, multiplication_or_sum, add_and_iterate, reverse_str, remove_first_n_chars, check_n_same, save_divisible
 
-@unittest.skip("Skipping TestMultiplicationOrSum")
+
 class TestMultiplicationOrSum(unittest.TestCase):
+    @unittest.skip("")
     def test_less_thousand(self):
         self.assertEqual(multiplication_or_sum(20, 50), 70)
 
+    @unittest.skip("")
     def test_more_thousand(self):
         self.assertEqual(multiplication_or_sum(10, 50), 500)
 
-@unittest.skip("Skipping TestEvenOrOdd")
+@unittest.skip("")
 class TestEvenOrOdd(unittest.TestCase):
     def test_even(self):
         self.assertEqual(even_or_odd(2), 1)
@@ -26,6 +28,36 @@ class TestEvenOrOdd(unittest.TestCase):
         self.assertEqual(even_or_odd("awdaw"), 0)
         self.assertEqual(even_or_odd(True), 0)
 
+
+class TestArr(unittest.TestCase):
+    @unittest.skip("")
+    def test_add_and_iterate(self):
+        self.assertEqual(add_and_iterate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 55)
+
+    @unittest.skip("")
+    def test_check_n_same(self):
+        self.assertEqual(check_n_same([1, 2, 3, 4, 5, 6, 7, 8], [1, 1, 1, 1, 1, 2, 3], 3), True)
+        self.assertEqual(check_n_same([1, 2, 3, 4, 5, 6, 7, 8], [1, 1, 1, 1, 3, 2, 3], 3), False)
+    
+    @unittest.skip("")
+    def test_save_divisible(self):
+        self.assertEqual(save_divisible([10, 12, 13, 30, 50], 5), [10, 30, 50])
+
+
+class TestStr(unittest.TestCase):
+    @unittest.skip("")
+    def test_reverse_str(self):
+        self.assertEqual(reverse_str("hello"), "olleh")
+
+    @unittest.skip("")
+    def test_remove_first_n_chars(self):
+        self.assertEqual(remove_first_n_chars("hello", 3), "lo")
+        self.assertEqual(remove_first_n_chars("hello", 6), "")
+        self.assertEqual(remove_first_n_chars("hello", 0), "")
+        self.assertRaises(TypeError, remove_first_n_chars, "hello", -1)
+        self.assertRaises(TypeError, remove_first_n_chars, "hello", "123")
+        self.assertRaises(TypeError, remove_first_n_chars, "hello", True)
+    
 
 if __name__ == '__main__':
     # suite = unittest.TestSuite()
