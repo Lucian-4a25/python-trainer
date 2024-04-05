576719189468fb639e2ba6fcb5ad7f52f1faaf3d
Lucian <lucian.journey@gmail.com>
2024-04-05 21:39:27 +0800

    add more flexiblable unit test

--- project_0.py
+++ project_0.py
@@ -1,8 +1,17 @@
-# Q1: 按照下面的要求，完成 function even_or_odd 的功能实现
+# Q1: 按照下面的要求，完成 function 的功能实现
+
+# - 如果输入的 num1 和 num2 的乘积大于等于 1000，那么返回这两个数的和
+# - 如果输入的 num1 和 num2 的乘积小于 1000，那么返回这两个数的乘积
+def multiplication_or_sum(num1, num2):
+  # 你的代码写在这里
+  0
+
 # - 如果输入是偶数则返回 1，如果输入是奇数则返回 2
 # - 如果输入非数字类型的参数，返回 0
 def even_or_odd(n):
+  # 你的代码写在这里
   n
+
 
 if __name__ == '__main__':
   print("Hello Python")
--- tests/test_project_0.py
+++ tests/test_project_0.py
@@ -4,8 +4,17 @@ sys.path.append("..")
 import unittest
 
 # print(sys.path)
-from project_0 import even_or_odd
+from project_0 import even_or_odd, multiplication_or_sum
 
+@unittest.skip("Skipping TestMultiplicationOrSum")
+class TestMultiplicationOrSum(unittest.TestCase):
+    def test_less_thousand(self):
+        self.assertEqual(multiplication_or_sum(20, 50), 70)
+
+    def test_more_thousand(self):
+        self.assertEqual(multiplication_or_sum(10, 50), 500)
+
+@unittest.skip("Skipping TestEvenOrOdd")
 class TestEvenOrOdd(unittest.TestCase):
     def test_even(self):
         self.assertEqual(even_or_odd(2), 1)
@@ -19,4 +28,12 @@ class TestEvenOrOdd(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.TestCase
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestMultiplicationOrSum('test_less_thousand'))
+    # suite.addTest(TestMultiplicationOrSum('test_more_thousand'))
+
+    # # 创建测试运行器
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite)
+
+    unittest.main()
